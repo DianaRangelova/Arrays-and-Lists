68565c047d3f089dd3a66dd648993bf296975f02
DianaRangelova <diana_kirilova@mail.bg>
2024-02-22 00:51:23 +0200

    Arrays and Lists in C#

---
+++ P05-Zig-Zag-Arrays/Program.cs
@@ -0,0 +1,28 @@
+﻿int count = int.Parse(Console.ReadLine());
+
+List<int> firstList = new List<int>();
+List<int> secondList = new List<int>();
+
+for (int i = 0; i < count; i++)
+{
+    List<int> numbers = Console.ReadLine()
+                     .Split(" ")
+                     .Select(int.Parse)
+                     .ToList();
+    int numb1 = numbers[0];
+    int numb2 = numbers[1];
+
+    if (i % 2 == 0)
+    {
+        firstList.Add(numb1);
+        secondList.Add(numb2);
+    }
+    else
+    {
+        firstList.Add(numb2);
+        secondList.Add(numb1);
+    }
+}
+
+Console.WriteLine(string.Join(" ", firstList));
+Console.WriteLine(string.Join(" ", secondList));
---
+++ P06-Array-Rotation/Program.cs
@@ -0,0 +1,16 @@
+﻿List<int> input = Console.ReadLine()
+                   .Split(" ")
+                   .Select(int.Parse)
+                   .ToList();
+
+int rotations = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < rotations; i++)
+{
+    int currentNumb = input[0];
+    input.RemoveAt(0);
+    input.Add(currentNumb);
+}
+
+Console.WriteLine(string.Join(" ", input));
+
